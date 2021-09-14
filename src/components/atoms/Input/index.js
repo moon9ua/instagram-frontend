3e11335f0447a157dca5a7f7b8287c6dac2f50ea
moon9ua <koom6@naver.com>
2021-09-15 02:10:58 +0900

    refactor: 백업용 주석 제거

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,53 +1,17 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-// width: ${({ theme }) => theme.inputWidths.home};
-// width: ${(props) => props.theme.inputWidths[props.size]};
-
-/*
-    return `
-      width: ${props.theme.inputWidths[theme]};
-      background-color: ${
-        theme === "home" ? props.theme.colors.backgroundGray : props.theme.colors.white
-      };
-      `;
-*/
-
 const StyledInput = styled.input`
   ${({ themeName, theme }) => {
     const chosenTheme = theme[themeName];
-
     return `
           width: ${chosenTheme.width};
           background-color: ${chosenTheme["background-color"]};
           padding: ${chosenTheme.padding};
           border: 1px solid ${chosenTheme["border-color"]};
           `;
-    /* border-color: ${chosenTheme["border-color"]}; */
-    /* switch (themeName) {
-      case "inputForHome":
-        return `
-          width: ${chosenTheme.width};
-          background-color: ${chosenTheme["background-color"]};
-          padding: ${chosenTheme.padding};
-          border-color: ${chosenTheme["border-color"]};
-        `;
-      case "inputForSearch":
-        return `
-          width: ${chosenTheme.width};
-          background-color: ${chosenTheme["background-color"]};
-          padding: ${chosenTheme.padding};
-          border-color: ${chosenTheme["border-color"]};
-        `;
-      default:
-        return "";
-    } */
   }};
-  /* padding: 10px; */
-  /* padding: 5px 0; */
   margin: 3px 0;
-  /* border-color: #dbdbdb; */
-  /* border: 1px solid; */
   border-radius: 3px;
   box-sizing: border-box;
 `;
@@ -68,11 +32,6 @@ const Input = ({ placeholder, name, themeName = "inputForHome" }) => {
       themeName={themeName}
     ></StyledInput>
   );
-  // return <StyledInput placeholder={placeholder} name={name}></StyledInput>;
 };
-
-Input.defaultProps = {
-  size: "home",
-}; // 이게 되나?
 
 export default Input;
