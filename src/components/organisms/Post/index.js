169e10a4f01f0ee2cdeea5e14afb9b07241db0f6
moon9ua <koom6@naver.com>
2021-09-24 16:38:37 +0900

    fix: Post CSS 수정

--- src/components/organisms/Post/index.js
+++ src/components/organisms/Post/index.js
@@ -12,18 +12,19 @@ const StyledDiv = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.borderGray};
   margin: 30px 0 0;
   width: 600px;
-  height: 800px;
   display: flex;
   flex-direction: column;
   background-color: #fff;
   /* background-color: purple; */
   border-radius: 3px;
   /* align-items: center; */
+`;
 
-  img {
-    border-top: 1px solid ${({ theme }) => theme.colors.borderGray};
-    border-bottom: 1px solid ${({ theme }) => theme.colors.borderGray};
-  }
+const Styledimg = styled.img`
+  width: 100%;
+  aspect-ratio: 1;
+  border-top: 1px solid ${({ theme }) => theme.colors.borderGray};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderGray};
 `;
 
 const Post = ({ username, img, id }) => {
@@ -65,7 +66,7 @@ const Post = ({ username, img, id }) => {
   return (
     <StyledDiv>
       <PostHeader username={username} src={user.images} />
-      <img src={img} alt="" />
+      <Styledimg src={img} alt="" />
       <PostIcons />
       <PostComments>
         {comments.map((val) => {
